73605bcbeef09a8f05f7bd40e3de40788c4d1367
Vivek Hari <vivek.hari@outlook.co.nz>
2018-07-29 18:57:30 +1200

    Add basic console app for agent.

---
+++ DistributedGrapher.Agent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DistributedGrapher.Agent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
